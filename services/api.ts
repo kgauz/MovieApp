378a1c1286a527ac7d694e9a447b5deba8a00d5b
kgauza <125618612+kgauz@users.noreply.github.com>
2026-03-12 19:11:42 +0200

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,9 +1,4 @@
-// API read acess
-// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjVmOTBkY2E5MzU2OWJiYmFmMWJjMzRlZTAyOTJkNyIsIm5iZiI6MTc2OTkzMDg2NS4yNzIsInN1YiI6IjY5N2YwMDcxNWJhZTlkZTRmMjFjNWFiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6rOKjLGoFLpDbzdWtTgeUM6RjHDQV84Z8dJNBun8trY
 
-
-// api_key
-// 4f5f90dca93569bbbaf1bc34ee0292d7
 
 export const TMDB_CONFIG= {
     Baseurl: 'https://api.themoviedb.org/3' ,
@@ -41,19 +36,5 @@ export const TMDB_CONFIG= {
       return data.results;
 }
 
-
-// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjVmOTBkY2E5MzU2OWJiYmFmMWJjMzRlZTAyOTJkNyIsIm5iZiI6MTc2OTkzMDg2NS4yNzIsInN1YiI6IjY5N2YwMDcxNWJhZTlkZTRmMjFjNWFiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6rOKjLGoFLpDbzdWtTgeUM6RjHDQV84Z8dJNBun8trY'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
 
 export default useFetchMovies;
