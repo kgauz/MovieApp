6c4eb7acc914ad5b25931d586a18f965652f82a2
kgauza <125618612+kgauz@users.noreply.github.com>
2026-03-12 19:13:18 +0200

    Update fetchTrending.ts

--- services/fetchTrending.ts
+++ services/fetchTrending.ts
@@ -1,10 +1,10 @@
 // fetchTrending.ts
- const BACKEND_URL = "http://localhost:5000"; // update for emulator/device
+ const BACKEND_URL = "https://movieapp-acny.onrender.com"; // update for emulator/device
 
 export default async function fetchTrending() {
   const res = await fetch(`${BACKEND_URL}/trending`);
   const data = await res.json();
-  console.log(data);
+ 
   return data;
 }
 
